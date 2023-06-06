21e47d9466abf7866db8381eaba4b6a2e49e8b73
shahab-salehi <s.salehi@domain.com>
2023-06-06 12:31:18 +0330

    try different method

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -13,13 +13,9 @@ Complexity = O(n)
 """
 
 # tl:dr -- array slicing by value
-def limit(arr, min_lim=None, max_lim=None):
-    if len(arr) == 0:
-        return arr
+def limit(arr, min=None, max=None):
+    min_check = lambda val : True if min is None else (val >= min)
+    max_check = lambda val : True if max is None else (val <= max)
+    return [val for val in arr if min_check(val) and max_check(val) ]
 
-    if min_lim is None:
-        min_lim = min(arr)
-    if max_lim is None:
-        max_lim = max(arr)
-
-    return list(filter(lambda x: (min_lim <= x <= max_lim), arr))
+print(limit ([1,2,3,4,5], None, 3))
